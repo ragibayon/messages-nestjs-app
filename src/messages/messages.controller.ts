66b9facfebc4f29239004ca139251cb7359c006e
ragibayon <ragib.ayon@live.com>
2023-09-29 14:55:08 +0600

    initial commit: messages app

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+  @Get('/')
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
+  }
+  @Post('/')
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+}
